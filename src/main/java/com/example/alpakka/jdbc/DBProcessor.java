e188ce2ae946b5d0956502ed433cf4c9bffc94b4
Markus Eisele <m@lightbend.com>
2018-01-25 08:28:35 +0100

    Fixes

--- src/main/java/com/example/alpakka/jdbc/DBProcessor.java
+++ src/main/java/com/example/alpakka/jdbc/DBProcessor.java
@@ -10,12 +10,8 @@ import akka.http.javadsl.model.ws.Message;
 import akka.http.javadsl.model.ws.TextMessage;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
-
-//#important-imports
 import akka.stream.javadsl.*;
 import akka.stream.alpakka.slick.javadsl.*;
-
-//#important-imports
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import java.util.List;
@@ -30,27 +26,27 @@ import java.util.function.Function;
 import java.util.logging.Level;
 
 /**
- *
  * @author myfear
  */
 public class DBProcessor {
 
+    // Actor system and materializer
     private static ActorSystem system = ActorSystem.create();
     private static Materializer materializer = ActorMaterializer.create(system);
 
+    // Logger and configuration
     private static final Logger LOGGER = Logger.getLogger("DBProcessor");
     private static final Config CONFIG = ConfigFactory.load();
     // Slick session
     private static final SlickSession SESSION = SlickSession.forConfig("slick-h2");
 
-    //users
-    private static final List<User> users = IntStream.range(0, 42).boxed().map((i) -> new User(i, "Name" + i)).collect(Collectors.toList());
+    //Inser Users
+    private static final List<User> users = IntStream.range(0, 50).boxed().map((i) -> new User(i, "Name" + i)).collect(Collectors.toList());
     private static final Function<User, String> insertUser = (user) -> "INSERT INTO USERS VALUES (" + user.id + ", '" + user.name + "')";
 
     //Slick Sources and Sinks
     final static Sink<User, CompletionStage<Done>> usersInsert = Slick.sink(SESSION, 4, insertUser);
-    final static Source<User, NotUsed> USERSSOURCE = Source.from(users);
-    final static CompletionStage<Done> insertionResultFuture = USERSSOURCE.runWith(usersInsert, materializer);
+    final static RunnableGraph<CompletionStage<Done>> inserUsersGraph = Source.from(users).toMat(usersInsert, Keep.right());
 
     final static Source<User, NotUsed> usersStream = Slick.source(
             SESSION,
@@ -60,11 +56,18 @@ public class DBProcessor {
 
     public static void main(String[] args) throws Exception {
         LOGGER.info("Init");
-
+        // close Slick session onTermination of Actor system
+        system.registerOnTermination(() -> {
+            LOGGER.info("termination of actor system");
+            SESSION.close();
+        });
+        // Load first 49 users
+        inserUsersGraph.run(materializer);
+        //Start Akka HTTP server
         LOGGER.info("Start Server");
-        DBProcessor processor = new DBProcessor();
-        Server server = new Server(processor.usersStream);
-        server.startServer(CONFIG.getString("server.host"), CONFIG.getInt("server.port"));
+        Server server = new Server(materializer, usersStream);
+        server.startServer(CONFIG.getString("server.host"), CONFIG.getInt("server.port"), system);
+        system.terminate();
 
     }
 
@@ -72,15 +75,17 @@ public class DBProcessor {
 
 class Server extends HttpApp {
 
+    private final Materializer materializer;
     private final Source<User, NotUsed> usersStream;
 
-    Server(Source<User, NotUsed> usersStream) {
+    Server(Materializer materializer, Source<User, NotUsed> usersStream) {
+        this.materializer = materializer;
         this.usersStream = usersStream;
     }
 
     ;
 
-  @Override
+    @Override
     protected Route routes() {
         return route(
                 path("data", () -> {
@@ -92,11 +97,11 @@ class Server extends HttpApp {
                 path("more", ()
                         -> {
                     try {
-                        DBProcessor.insertionResultFuture.toCompletableFuture().get(5, TimeUnit.SECONDS);
+                        DBProcessor.inserUsersGraph.run(materializer).toCompletableFuture().get(5, TimeUnit.SECONDS);
                     } catch (InterruptedException | ExecutionException | TimeoutException ex) {
                         Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    return complete(StatusCodes.OK, "Ok");
+                    return complete(StatusCodes.OK);
                 }),
                 get(()
                         -> pathSingleSlash(()
